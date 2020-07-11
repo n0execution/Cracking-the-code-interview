c9daf91ecbb0351b6500723ecda45a14f47772d6
dimasmemas <dimichkit@gmail.com>
2020-07-11 14:36:11 +0300

    Added Linked List for Tree Nodes in go

--- Trees_Graphs/go/Tree.go
+++ Trees_Graphs/go/Tree.go
@@ -1,8 +1,75 @@
 package types
 
 
+import (
+  "fmt"
+)
+
+
 type TreeNode struct {
   Value int
   LeftChild *TreeNode
   RightChild *TreeNode
+}
+
+
+func getChild(array []int) (*TreeNode) {
+  length := len(array)
+  node := &TreeNode{Value: array[length / 2]}
+
+  if length == 1 {
+    return node
+  }
+
+  if length == 2 {
+    node.LeftChild = getChild(array[:length / 2])
+  } else {
+    node.LeftChild, node.RightChild = getChild(array[:length / 2]), getChild(array[length / 2 + 1:])
+  }
+
+  return node
+}
+
+
+func CreateTree(array []int) *TreeNode {
+  length := len(array)
+  root := &TreeNode{Value: array[length / 2]}
+
+  root.LeftChild, root.RightChild = getChild(array[:length / 2]), getChild(array[length / 2  + 1:])
+
+  return root
+}
+
+
+type LinkedListTreeNode struct {
+  Node *TreeNode
+  Next *LinkedListTreeNode
+}
+
+
+type LinkedListOfTreeNodes struct{
+  Head *LinkedListTreeNode
+  Tail *LinkedListTreeNode
+}
+
+
+func (list *LinkedListOfTreeNodes) Append(node *TreeNode) {
+  newNode := LinkedListTreeNode{Node: node}
+  if list.Head == nil {
+    list.Head = &newNode
+    list.Tail = &newNode
+  } else {
+    list.Tail.Next = &newNode
+    list.Tail = list.Tail.Next
+  }
+}
+
+
+func PrintLinkedListOfTreeNodes(list LinkedListOfTreeNodes) {
+  node := list.Head
+  for node != nil {
+    fmt.Print(node.Node.Value, "->")
+    node = node.Next
+  }
+  fmt.Print("nil\n")
 }
